103c5f8961815b43c4b2e8283769d4718dd58159
Brian Faherty <anothergenericuser@gmail.com>
2014-02-28 15:11:09 -0500

    fix for cache prefix default

--- pipes/cache.js
+++ pipes/cache.js
@@ -3,9 +3,10 @@ var redis = require('koa-redis')
 module.exports = function(route) {
   var store = redis();
   var timeout = route.pipeline.cache.timeout || 15000;
+  var prefix = route.pipeline.cache.prefix || "";
 
   return function *(next) {
-    var hash_key = route.pipeline.cache.prefix.concat(this.request.url);
+    var hash_key = prefix.concat(this.request.url);
     var cache_lookup_result = yield store.get(hash_key);
     if (cache_lookup_result) {
       this.body = cache_lookup_result;
